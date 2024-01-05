42be3bb0d86503998ecd784b91ccf4203815b929
nbgarrett88 <nbgarrett88@gmail.com>
2024-01-05 16:16:59 -0600

    Solved Day 3-1

--- 3.py
+++ 3.py
@@ -1,32 +1,21 @@
-def find_special_chars(line):
-    return set([x for x in line if not x.isnumeric()])
-
-def remove_special_chars(line):
-    
-    for char in find_special_chars(line):
-        if not char == '.':
-            line = line.replace(char,'.')
-
-    return line
-
 def get_scan_index(line):
 
     scan_ixs = {}
-    nums = []
     ixs = []
+    nums = ''
 
     for ix, x in enumerate(line):
         if x.isnumeric():
-            nums.append(x)
             ixs.append(ix)
-            
+            nums += x
+
             if len(ixs) > 1:
                 if ixs[-1]-ixs[-2]>1:
-                    scan_ixs[(''.join(nums[:-1])),ixs[0]] = ixs[:-1]
-                    nums = nums[-1:]
+                    scan_ixs[nums[:-1],ixs[0]] = ixs[:-1]
                     ixs = ixs[-1:]
+                    nums = nums[-1]
     if ixs:
-        scan_ixs[(''.join(nums)),ixs[0]] = ixs
+        scan_ixs[nums,ixs[0]] = ixs
             
     return scan_ixs
 
@@ -45,10 +34,7 @@ def score_rows(lines):
 
     score = 0
     for i in range(1, len(lines)):
-
-        scan_ixs = get_scan_index(remove_special_chars(lines[i]))
-        
-        for num, ixs in scan_ixs.items():
+        for num, ixs in get_scan_index(lines[i]).items():
             for ix in ixs:
                 if ix < len(lines[i])-1:
                     if scan_window(lines, i, num, ix) == 0:
